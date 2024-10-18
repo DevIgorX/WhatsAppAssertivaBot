6289d3af95ea7f51a3b58c9fd69f86c4cd0dabfe
Tauam Igor <tauamigor@gmail.com>
2024-10-18 18:25:41 -0300

    configurando funcionalidades do bot

--- src/Whatsapp/bot.ts
+++ src/Whatsapp/bot.ts
@@ -3,10 +3,9 @@ import {create, Whatsapp} from 'venom-bot'
 
 
  export const startBot = () =>{
-    create({
-        session: 'Whataspp-bot'
-     }).then((client)=>{
+    create({session: 'Whataspp-bot'}).then((client)=>{
         start(client)
+        
      }).catch((erro:any) =>{
         console.log(erro)
      });
@@ -16,10 +15,19 @@ import {create, Whatsapp} from 'venom-bot'
  
 function start(client:any){
     client.onMessage((message:any)=>{
-        if(message.body === 'Oi'){
-            client.sendText(message.from, 'Olá! como posso ajudar?')
+        if(message.body != "" && message.isGroupMsg === false){
+           client.sendText(message.from, `Olá como posso te ajudar hoje?\n1 1 - Mais contatos? \n2 2- Endereços`)
+           if(message.body === '1'){
+            client.sendText(message.from, 'Segue contato: \n1 62992626968')
+           }else if(message.body === '2'){
+            client.sendText(message.from, 'Segue Endereço: \n1 Rua 3, Setor São José')
+           }
+
+           
         }
-    })
+
+       
+    });
 }
 
 
