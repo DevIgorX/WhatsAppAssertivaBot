5359a5ee9a1c1fde31c825c98654f64b118c6493
Tauam Igor <tauamigor@gmail.com>
2024-10-27 14:44:23 -0300

    melhorando a leitura do codigo com funcoes assincronas

--- src/Whatsapp/bot.ts
+++ src/Whatsapp/bot.ts
@@ -3,67 +3,92 @@ import { create } from 'venom-bot'
 import { consulta_telefone } from './controladores';
 
 
-export const startBot = () => {
-    create({ session: 'Whataspp-bot' }).then((client) => {
+//export const startBot = () => {
+//create({ session: 'Whataspp-bot' }).then((client) => {
+//   start(client)
+
+// }).catch((erro: any) => {
+// console.log(erro)
+//});
+//}
+
+export const startBot = async () => {
+    try {
+
+        const client = await create({ session: 'Whatsapp-bot' })
+
         start(client)
 
-    }).catch((erro: any) => {
-        console.log(erro)
-    });
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
 // Objeto para armazenar o estado atual de cada usuário //objeto com chave-valor
 
-const usuarioEstdo: {[chave: string]: string}= {}
+const usuarioEstdo: { [chave: string]: string } = {}
 
 function start(client: any) {
-    client.onMessage((message: any) => {
+    client.onMessage(async (message: any) => {
         // Verifica se a mensagem não está vazia e não é de um grupo
         if (message.body != "" && message.isGroupMsg === false) {
-            
+
             //pega o estado atual do usuario, se existir
 
             const estadoAtual = usuarioEstdo[message.from] || 'inicial'
 
 
-            if(estadoAtual === 'inicial'){
+            if (estadoAtual === 'inicial') {
                 //Estado inicial: Pergunta comoo usuário quer ser ajudado
-                client.sendText(message.from, `Olá, como posso te ajudar hoje?\n 1 - Mais contatos? \n 2 - Endereços`);
+                await client.sendText(message.from, `Olá, como posso te ajudar hoje?\n 1 - Mais contatos? \n 2 - Endereços`);
                 //atualiza o estado do usuario
 
                 usuarioEstdo[message.from] = 'aguardando_opção'
-            }else if(estadoAtual === 'aguardando_opção'){
+            } else if (estadoAtual === 'aguardando_opção') {
                 //bot está aguardando uma resposta da primeira pergunta 
-                if(message.body === '1'){
-                    client.sendText(message.from,'Por favor digite o seu CPF')
+                if (message.body === '1') {
+                    await client.sendText(message.from, 'Por favor digite o seu CPF')
                     //Atualiza o estado para aguardando o CPF
-                    usuarioEstdo[message.from] =  'aguardando_cpf'
-                }else if(message.body === '2'){
-                    client.sendText(message.from, 'Segue endereço: \n Rua 3, Setor São josé')
+                    usuarioEstdo[message.from] = 'aguardando_cpf'
+                } else if (message.body === '2') {
+                    await client.sendText(message.from, 'Segue endereço: \n Rua 3, Setor São josé')
                     //volta o estado para inicial depois de completar a ação
                     usuarioEstdo[message.from] = 'inicial'
-                }else {
+                } else {
                     //se o usuário não digitar uma opção válida
-                    client.sendText('Por favor ,escolha uma opção válida: 1 ou 2')
+                    await client.sendText('Por favor ,escolha uma opção válida: 1 ou 2')
                 }
-            }else if(estadoAtual === 'aguardando_cpf'){
+            } else if (estadoAtual === 'aguardando_cpf') {
 
-                consulta_telefone(message.body).then((contatos)=>{
-                     const contatosFormatados = contatos.join('\n'); // formata a lista de contatos em multiplas linhas
-                     client.sendText(message.from, `Segue contatos:\n${contatosFormatados}`)
-                     //volta ao estado para inicial 
-                     usuarioEstdo[message.from] = 'inicial'
-                }).catch((erro)=>{
-                    console.log('Erro ao consultar o telefone:', erro)
+                try {
+
+                    //consulta_telefone(message.body).then((contatos)=>{
+                    //const contatosFormatados = contatos.join('\n'); // formata a lista de contatos em multiplas linhas
+                    //client.sendText(message.from, `Segue contatos:\n${contatosFormatados}`)
+                    //volta ao estado para inicial 
+                    //usuarioEstdo[message.from] = 'inicial'
+                    //})
+
+
+                    const contatos = await consulta_telefone(message.body)
+
+                    const contatosFormatados = contatos.join('\n') //retorna uma string unica usando uma nova linha como separador dos contatos
+
+                    await client.sendText(message.from, `Segue contatos:\n${contatosFormatados}`)
+                    usuarioEstdo[message.from] = 'inicial'
+
+
+
+                } catch (error) {
+                    console.log('Erro ao consultar o telefone:', error)
                     client.sendText(message.from, 'Desculpe, não conseguimos processar seu CPF no momento.')
                     usuarioEstdo[message.from] = 'inicial'
-                })
-
+                }
             }
 
 
-           
+
         }
     });
 }
